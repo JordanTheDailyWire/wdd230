792747d500c251be641b1ae91c40d49345ba1395
JordanTheDailyWire <cap21014@byui.edu>
2022-06-25 08:54:20 -0700

    Update

--- lesson4/chamber/javascript/index.js
+++ lesson4/chamber/javascript/index.js
@@ -1,15 +1,15 @@
 // select the elements to manipulate (output to)
-const datefield = document.querySelector(".date-1");
+const datefield = document.querySelector(".date");
+const datefield1 = document.querySelector(".date-1");
 const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
 
 // derive the current date using a date object
 const now = new Date();
-const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
-	now
-);
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
 // long, medium, short options ... try them
 
 datefield.innerHTML = `<em>${fulldate}</em>`;
+datefield1.innerHTML = `<em>${fulldate}</em>`;
 datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
 
 document.querySelector('#year').innerHTML = new Date().getFullYear();
