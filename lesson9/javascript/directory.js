954e78373a90fc7fc9870a65cb4ba61dde402d4f
JordanTheDailyWire <cap21014@byui.edu>
2022-09-16 23:47:48 -0700

    Update.

--- lesson9/javascript/directory.js
+++ lesson9/javascript/directory.js
@@ -1,92 +1,81 @@
-const dataURL = "https://github.com/JordanTheDailyWire/wdd230/blob/main/lesson9/json/data.json";
-const listDiv = document.querySelector('.list-view');
-const cardDiv = document.querySelector('.grid-view');
+const requestURL = "https://raw.githubusercontent.com/JordanTheDailyWire/wdd230/main/lesson9/json/data.json";
 
-fetch(dataURL)
-    .then((response) => {
-        return response.json();
-    })
-    .then((jsonObject) => {
-        console.table(jsonObject);
-
-        const businesses = jsonObject['businesses'];
-        businesses.forEach(displayBusinessesInGrid);
-        businesses.forEach(displayBusinessesInList);
+fetch(requestURL)
+.then(function(response) {
+    return response.json();
+})
+.then(function(jsonObject) {
+    console.log(jsonObject);
+    const businesses = jsonObject["businesses"];
+    businesses.forEach(displayBusinesses);
     });
 
-    function displayBusinessesInGrid(business) {
+function displayBusinesses(business) {
+    let card = document.createElement("section");
+    let name = document.createElement("name");
+    let phone = document.createElement("phone");
+    let admin = document.createElement("admin");
+    let email = document.createElement("email");
+    let img = document.createElement("img");
+    let member = document.createElement("member");
 
-        let media_card = document.createElement('section');
-        let h2 = document.createElement('h2');
-        let image = document.createElement('img');
-        let hr = document.createElement('hr');
-        let phone = document.createElement('p');
-        let address = document.createElement('p');
-        let website = document.createElement('a');
+    let li = document.createElement('section');
+    let listName = document.createElement('name');
+    let listPhone = document.createElement('phone');
+    let listAdmin = document.createElement('admin');
+    let listEmail = document.createElement("email");
+    let listMember = document.createElement("member");
 
-        h2.textContent = `${business.name}`
-        phone.textContent = `${business.phone}`
-        address.textContent = `${business.address}`
-        website.textContent = `${business.website}`
-        
-        website.setAttribute("href", business.website);
-        image.setAttribute("src", business.images);
-        image.setAttribute("alt", `Image of ${business.name}`);
-        image.setAttribute("loading", "lazy");
-       
-        media_card.appendChild(h2);
-        media_card.appendChild(image);
-        media_card.appendChild(hr);
-        media_card.appendChild(phone);
-        media_card.appendChild(address);
-        media_card.appendChild(website);
-            
-        cardDiv.appendChild(media_card);
-    }
+    name.textContent = `${business.name} ${business.lastname}`;
+    phone.textContent = `${business.phone}`;
+    admin.textContent = `${business.admin}`;
+    email.textContent = `${business.email}`;
+    member.textContent = `${business.member}`;
+    listName.textContent = `${business.name} ${business.lastname}`;
+    listPhone.textContent = `${business.phone}`;
+    listAdmin.textContent = `${business.admin}`;
+    listEmail.textContent = `${business.email}`;
+    listMember.textContent = `${business.member}`;
 
-    function displayBusinessesInList(business) {
+    img.setAttribute('src', business.imageurl);
+    img.setAttribute('alt', `${business.name} ${business.lastname}`);
+    
+    card.classList.add(`card`);
+    img.classList.add(`card-img`);
 
-        let media_card = document.createElement('section');
-        let h2 = document.createElement('h2');
-        let phone = document.createElement('p');
-        let address = document.createElement('p');
-        let website = document.createElement('p');
+    card.appendChild(name);
+    card.appendChild(phone);
+    card.appendChild(admin);
+    card.appendChild(email);
+    card.appendChild(member);
+    card.appendChild(img);
+    li.appendChild(listName);
+    li.appendChild(listPhone);
+    li.appendChild(listAdmin);
+    li.appendChild(listEmail);
+    li.appendChild(listMember);
+    document.querySelector(`div.cards`).appendChild(card);
+    document.querySelector(`div.list`).appendChild(li);
+};
 
-        h2.textContent = business.name
-        phone.textContent = business.phone
-        address.textContent = business.address
-        website.textContent = business.website
-        
-       
-        media_card.appendChild(h2);
-        media_card.appendChild(phone);
-        media_card.appendChild(address);
-        media_card.appendChild(website);
-            
-        listDiv.appendChild(media_card);
-    }
+const cardButton = document.querySelector(".grid-active");
+const listButton = document.querySelector(".list");
 
-// Script for Grid and List Views
-let viewsButtons = document.querySelectorAll('.links ul li');
-let views = document.querySelectorAll('.view-div');
+const Card = document.querySelector(".cards");
+const List = document.querySelector(".List");
 
-viewsButtons.forEach((link) => {
-    link.addEventListener('click', () => {
-        viewsButtons.forEach((item) => {
-            item.classList.remove('active');
-        })
-        link.classList.add('active');
-        
-        let li_view = link.getAttribute('data-view');
+cardButton.onclick = function() {
+    const cardBox = document.querySelector(".cards");
+    const listBox = document.querySelector(".list");
 
-        views.forEach((view) => {
-            view.style.display = 'none';
-        })
-        
-        if (li_view == 'grid-view') {
-            document.querySelector('.' + li_view).style.display = 'grid';
-        } else {
-            document.querySelector('.' + li_view).style.display = 'block';
-        }
-    })
-})
+    cardBox.style.display = "flex";
+    listBox.style.display = "none";
+};
+
+listButton.onclick = function() {
+    const cardBox = document.querySelector(".cards");
+    const listBox = document.querySelector(".list");
+
+    cardBox.style.display = "none";
+    listBox.style.display = "flex";
+};
